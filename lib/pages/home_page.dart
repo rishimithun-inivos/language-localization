2d18403d51ed8cd86529bc33d6ea946d0cbae14c
Rishimithun Muralidharan <rishimithun.muralidaran@inivossl.com>
2023-10-17 14:49:04 +0530

    add text body

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localization/flutter_localization.dart';
+import 'package:localization_sample/localization/locales.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          LocaleData.title.getString(
+            context,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 50,
+        ),
+        child: Text(
+          context.formatString(
+            LocaleData.body,
+            [],
+          ),
+          style: TextStyle(
+            fontSize: 21,
+          ),
+        ),
+      ),
+    );
   }
 }
